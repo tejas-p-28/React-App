781cd5a8730a62f5fb81545cece5383284de5157
Tejas <tejas.patiltp2802@gmail.com>
2022-03-01 22:34:41 +0530

    React

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Footer(){
+    return(
+        <div>
+            <hr/>
+            <center>
+                <h3>&copy; Tejas Developer</h3>
+            </center>
+        </div>
+    )
+}
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = () => {
+    return(
+        <div>
+            <center>
+                <h1>Tejas Patil</h1>
+            </center>
+            <hr></hr>
+        </div>
+    )
+}
+export default Header
